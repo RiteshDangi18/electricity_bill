bcdf84e597960f90848981c51399aef0bcedfd82
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2022-12-08 22:16:27 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,24 @@
+#write a function to calculate the amount of electricity bill
+class bill:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+    def amount(self):
+        if 0<=self.x<=100:
+            print("address: ",self.y)
+            self.z=self.x*12
+            print("amount=",self.z)
+        elif 0<=self.x<=200:
+            print("address: ",self.y)
+            self.z=(self.x-100)*17+100*12
+            print("amount=",self.z)
+        else:
+            print("address: ",self.y)
+            self.z=(self.x-200)*25+100*17+100*12
+            print("amount=",self.z)
+
+x=int(input("enter the no of unit consumed: "))
+y=input("enter the address: ")
+a=bill(x,y,z=0)
+a.amount()
